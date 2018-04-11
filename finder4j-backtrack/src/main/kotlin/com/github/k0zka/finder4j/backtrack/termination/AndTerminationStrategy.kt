b35db8a187bf321411678a669a88811ca8933c84
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2018-04-11 07:35:34 +0200

    cleanup to make it more kotlin-like

--- finder4j-backtrack/src/main/kotlin/com/github/k0zka/finder4j/backtrack/termination/AndTerminationStrategy.kt
+++ finder4j-backtrack/src/main/kotlin/com/github/k0zka/finder4j/backtrack/termination/AndTerminationStrategy.kt
@@ -1,15 +1,8 @@
 package com.github.k0zka.finder4j.backtrack.termination
 
 import com.github.k0zka.finder4j.backtrack.TerminationStrategy
-import java.util.ArrayList
 
-class AndTerminationStrategy<X : Any>(strategies: List<TerminationStrategy<X>>) : TerminationStrategy<X> {
-
-	private val strategies: List<TerminationStrategy<X>>
-
-	init {
-		this.strategies = ArrayList(strategies)
-	}
+class AndTerminationStrategy<X : Any>(private val strategies: List<TerminationStrategy<X>>) : TerminationStrategy<X> {
 
 	override fun stop(state: X): Boolean {
 		for (strategy in strategies) {
